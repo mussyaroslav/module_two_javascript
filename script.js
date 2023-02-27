988e632629bf7d0baa2ebcdf3c6d565217f9bab8
mussyaroslav <kiborg1101@gmail.com>
2023-02-27 13:58:37 +0500

    Authorization

--- script.js
+++ script.js
@@ -1,37 +1,63 @@
-let getUsers = fetch('https://reqres.in/api/users?per_page=12')
-  .then((e) => {
-    return e.json()
-  })
+let submit = document.querySelector('#btn');
+let email = document.querySelector('#email');
+let password = document.querySelector('#password');
+let checkbox = document.querySelector('#custom-checkbox');
 
-  getUsers.then((result) => {
-    let allUsers = result.data
-    console.log(allUsers)
+let errorEmail = document.querySelector('#error-email');
+let errorPassoword = document.querySelector('#error-password');
+let errorCheckbox = document.querySelector('#error-checkbox');
 
-    // Вывод всех фамилий пункт 2
-    console.log('---------------');
-    console.log('Пункт 2');
-    allUsers.forEach(item => {
-      console.log(item.last_name);
-    });
-    console.log('---------------');
+let emailData = '';
+let passwordData = '';
+let checkboxData = '';
+let userData = {};
 
-    // Вывод всех фамилий которые начинатся на F пункт 3
-    console.log('Пункт 3');
-    allUsers.forEach(item => {
-      if(item.last_name.startsWith('F') == true) {
-        console.log(item)
-      }
-    });
-    console.log('---------------');
+email.addEventListener('input', (event) => {
+  emailData = event.target.value;
+})
 
-    // Вывод имени и фамилии с помощью reduce пункт 4
-    console.log('Пункт 4');
-    let names = allUsers.reduce((a,b,i) => a += `${b.first_name} ${b.last_name}${i !== allUsers.length-1 ? ', ' : ''}`,''); 
-    console.log(`Наша база содержит данные следующих пользователей: ${names}`); 
+password.addEventListener('input', (event) => {
+  passwordData = event.target.value;
+})
 
-    // Пункт 5
-    console.log('Пункт 5');
-    allUsers.forEach((item) => {
-      console.log(Object.keys(item))
-    })
-  })
+checkbox.addEventListener('input', (event) => {
+  checkboxData = event.target.value;
+})
+
+function validateEmail(emailData) {    
+  const re =/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;    
+  return re.test(String(emailData).toLowerCase());
+}
+
+submit.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  if (emailData == '') {
+    errorEmail.innerHTML = 'Поле обязательно для заполнения';
+  } else if (validateEmail(emailData) == false) {
+    errorEmail.innerHTML = 'Email невалидный';
+  } else {
+    errorEmail.innerHTML = '';
+    userData.email = emailData;
+  }
+
+  if (passwordData == '') {
+    errorPassoword.innerHTML = 'Поле обязательно для заполнения';
+  } else if (passwordData.length < 8) {
+    errorPassoword.innerHTML = 'Пароль должен содержать как минимум 8 символов';
+  }
+  else {
+    errorPassoword.innerHTML = '';
+    userData.password = passwordData;
+  }
+
+  if (checkboxData == '') {
+    errorCheckbox.innerHTML = 'Поле обязательно для заполнения';
+  } else {
+    errorCheckbox.innerHTML = '';
+  }
+
+  if ((emailData != '' && validateEmail(emailData) != false) && (passwordData != '' && passwordData.length > 8)) {
+    console.log(userData)
+  }
+})
